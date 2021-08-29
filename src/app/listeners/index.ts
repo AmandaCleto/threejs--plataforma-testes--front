4ed83b6533002974ec9c883903347cf76eeacb10
caioliveira277 <caio.oliveira277@etec.sp.gov.br>
2021-08-29 17:33:45 -0300

    changed index listeners
    Co-authored-by: AmandaCleto <amanda.clet@gmail.com>

--- src/app/listeners/index.ts
+++ src/app/listeners/index.ts
@@ -1,5 +1,8 @@
+import { PerspectiveCamera, WebGLRenderer } from "three";
 import Resize from "./Resize";
 
-export {
-    Resize
-};
+export default class Listeners {
+    constructor(camera: PerspectiveCamera, renderer: WebGLRenderer) {
+        new Resize(camera, renderer);
+    }
+}
